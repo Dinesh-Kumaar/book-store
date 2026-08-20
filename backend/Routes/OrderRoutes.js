d3d594153d17f1565c240fffb2cf6441728d669b
Dinesh-Kumaar <dineshkumaar0412@gmail.com>
2026-08-20 15:10:59 +0530

    small change

--- backend/Routes/OrderRoutes.js
+++ backend/Routes/OrderRoutes.js
@@ -6,7 +6,7 @@ const userAuth = require("../Middleware/authMiddleware");
 
 const {addToOrder, getOrder, getAllOrders,updateOrderStatus, updateOrder, deleteOrder} = require('../Controller/OrderController')
 router.post('/create-order', userAuth,  addToOrder);
-router.get('/get-order/', userAuth,  getOrder);
+router.get('/get-order', userAuth,  getOrder);
 router.get('/get-all-orders', adminAuth, getAllOrders);
 router.put('update-order-status/:id', adminAuth, updateOrderStatus);
 router.put('/update-order/:id', userAuth,  updateOrder);
